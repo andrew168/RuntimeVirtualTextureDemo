334f132fedfcc9ba77921db93ae31912c035200b
DEDE-Game <Billiondollarsassets@gmail.com>
2023-05-30 22:46:47 +0800

    Main

--- Blood_CH_CompeleteVirsion/Source/Blood_CH/Blood_CH.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Blood_CH.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Blood_CH, "Blood_CH" );
